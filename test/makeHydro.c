a31d948341d590793993284029b260d12befef01
gigachad0209 <97180047+gigachad0209@users.noreply.github.com>
2023-05-02 22:14:54 +0700

    Create makeHydro.c

---
+++ test/makeHydro.c
@@ -0,0 +1,11 @@
+#include "../userprog/syscall.h"
+#define MAX_ARR 100
+
+// copy from file_src to file_dst
+int main() {
+    
+    Signal("make_H");
+    Signal("make_H");
+    Write("make 2h\n", 8, Console_Output);
+    return 0;
+}
